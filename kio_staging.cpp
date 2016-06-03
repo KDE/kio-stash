368d763b47059314cf6d274f5748c4c5e83f2ec2
arnav dhamija <arnav.dhamija@gmail.com>
2016-06-03 17:50:20 +0530

    Wrote up code for preparing UDSEntries and listing the top level directory

--- kio_staging.cpp
+++ kio_staging.cpp
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by Arnav Dhamija <arnav.dhamija@gmail.com>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+
 #include "kio_staging.h"
 #include <QDebug>
 #include <QMimeType>
@@ -26,7 +45,6 @@ extern "C" {
 
 Staging::Staging(const QByteArray &pool, const QByteArray &app) : KIO::ForwardingSlaveBase("staging", pool, app)
 {
-    qDebug() << "Finished crying";
 }
 
 bool Staging::rewriteUrl(const QUrl &url, QUrl &newUrl) //don't fuck around with this
@@ -38,14 +56,108 @@ bool Staging::rewriteUrl(const QUrl &url, QUrl &newUrl) //don't fuck around with
     return true;
 }
 
+void Staging::listRoot()
+{
+    KIO::UDSEntry entry;
+    QString fileName;
+    QString filePath;
+    for (auto listIterator = m_List.begin(); m_List.begin() != m_List.end(); ++listIterator) {
+        filePath = listIterator->path();
+        fileName = QFileInfo(listIterator->url()).fileName();
+        qDebug() << fileName;
+        entry.clear();
+        createRootUDSEntry(entry, filePath, fileName, fileName);
+        listEntry(entry);
+    }
+}
+
+void Staging::createRootUDSEntry( KIO::UDSEntry &entry, const QString &physicalPath, const QString &displayFileName, const QString &internalFileName) //needs a lot of changes imo
+{
+    QByteArray physicalPath_c = QFile::encodeName(physicalPath);
+    QT_STATBUF buff;
+    if (QT_LSTAT(physicalPath_c, &buff) == -1) {
+        qWarning() << "couldn't stat " << physicalPath;
+        return;
+    }
+    if (S_ISLNK(buff.st_mode)) {
+        char buffer2[ 1000 ];
+        int n = readlink(physicalPath_c, buffer2, 999);
+        if (n != -1) {
+            buffer2[ n ] = 0;
+        }
+
+        entry.insert(KIO::UDSEntry::UDS_LINK_DEST, QFile::decodeName(buffer2));
+        // Follow symlink
+        // That makes sense in kio_file, but not in the trash, especially for the size
+        // #136876
+#if 0
+        if (KDE_stat(physicalPath_c, &buff) == -1) {
+            // It is a link pointing to nowhere
+            buff.st_mode = S_IFLNK | S_IRWXU | S_IRWXG | S_IRWXO;
+            buff.st_mtime = 0;
+            buff.st_atime = 0;
+            buff.st_size = 0;
+        }
+#endif
+    }
+
+    mode_t type = buff.st_mode & S_IFMT; // extract file type
+    mode_t access = buff.st_mode & 07777; // extract permissions
+    //access &= 07555; // make it readonly, since it's in the trashcan
+    Q_ASSERT(!internalFileName.isEmpty());
+    entry.insert(KIO::UDSEntry::UDS_NAME, internalFileName);   // internal filename, like "0-foo"
+    entry.insert(KIO::UDSEntry::UDS_DISPLAY_NAME, displayFileName);   // user-visible filename, like "foo"
+    entry.insert(KIO::UDSEntry::UDS_FILE_TYPE, type);
+    //if ( !url.isEmpty() )
+    //    entry.insert( KIO::UDSEntry::UDS_URL, url );
+
+    QMimeDatabase db;
+    QMimeType mt = db.mimeTypeForFile(physicalPath);
+    if (mt.isValid()) {
+        entry.insert(KIO::UDSEntry::UDS_MIME_TYPE, mt.name());
+    }
+    entry.insert(KIO::UDSEntry::UDS_ACCESS, access);
+    entry.insert(KIO::UDSEntry::UDS_SIZE, buff.st_size);
+    entry.insert(KIO::UDSEntry::UDS_MODIFICATION_TIME, buff.st_mtime);
+    entry.insert(KIO::UDSEntry::UDS_ACCESS_TIME, buff.st_atime);   // ## or use it for deletion time?
+    /*QMimeDatabase db;
+    QMimeType mt = db.mimeTypeForFile(physicalPath);
+    if (mt.isValid()) {
+        entry.insert(KIO::UDSEntry::UDS_MIME_TYPE, mt.name());
+    }
+    entry.insert(KIO::UDSEntry::UDS_LINK_DEST, physicalPath);
+    entry.insert(KIO::UDSEntry::UDS_DISPLAY_NAME, displayFileName);
+    entry.insert(KIO::UDSEntry::UDS_NAME, internalFileName);
+    entry.insert(KIO::UDSEntry::UDS_TARGET_URL, physicalPath);*/
+}
+
 void Staging::listDir(const QUrl &url)
 {
-    KIO::ForwardingSlaveBase::listDir(QUrl("file:///home/nic/gsoc-2016"));
+    //KIO::ForwardingSlaveBase::listDir(QUrl("file:///home/nic/gsoc-2016"));
+    QString tmp = url.path();
+    //dirty parsing hack, will fix later
+    tmp.replace(0, QString("staging:").length(), "");
+    if (tmp.isEmpty() || tmp == QString("/")) {
+        listRoot();
+    } else {
+        qDebug() << tmp;
+    }
+    finished();
 }
 
 void Staging::rename(const QUrl &src, const QUrl &dest, KIO::JobFlags flags)
 {
     KIO::ForwardingSlaveBase::rename(src, dest, flags);
+}
+
+bool Staging::checkURL(const QUrl &url)
+{
+    for(auto listIterator = m_List.begin(); listIterator != m_List.end(); listIterator++) {
+        if(listIterator->url() == url.url()) {
+            return true;
+        }
+    }
+    return false;
 }
 
 #include "kio_staging.moc"
--- kio_staging.h
+++ kio_staging.h
@@ -1,3 +1,22 @@
+/***************************************************************************
+ *   Copyright (C) 2016 by Arnav Dhamija <arnav.dhamija@gmail.com>         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA            *
+ ***************************************************************************/
+ 
 #ifndef KIO_STAGING_H
 #define KIO_STAGING_H
 
@@ -7,14 +26,18 @@ class Staging : public KIO::ForwardingSlaveBase
 {
     Q_OBJECT
 public:
+protected:
+    QList<QUrl> m_List;
+public:
     Staging(const QByteArray &pool, const QByteArray &app);
     ~Staging(){};
-    void listDir(const QUrl &url) Q_DECL_OVERRIDE;
-    //void prepareUDSEntry(KIO::UDSEntry &entry, bool listing=false) const;
-
-    void rename(const QUrl &, const QUrl &, KIO::JobFlags flags) Q_DECL_OVERRIDE;
 protected:
+    void listDir(const QUrl &url) Q_DECL_OVERRIDE;
+    void listRoot();
+    void createRootUDSEntry( KIO::UDSEntry &entry, const QString &physicalPath, const QString &displayFileName, const QString &internalFileName);
+    void rename(const QUrl &, const QUrl &, KIO::JobFlags flags) Q_DECL_OVERRIDE;
     bool rewriteUrl(const QUrl &url, QUrl &newUrl) Q_DECL_OVERRIDE;
+    bool checkURL(const QUrl &url);
 };
 
 #endif
