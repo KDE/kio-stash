cb4d55ae2350c12427a9a59685dc32e647f52933
Luigi Toscano <luigi.toscano@tiscali.it>
2017-06-29 09:14:10 +0200

    Use the common pattern for the name of the gettext template

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.ui -o -name \*.rc -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp` -o $podir/kio-stash.pot
+$XGETTEXT `find . -name \*.cpp` -o $podir/kio5_stash.pot
 rm -f rc.cpp
