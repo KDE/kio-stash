7c5726a7da56b91b97c0b6a17c714e7c0896e0cb
Luigi Toscano <luigi.toscano@tiscali.it>
2017-06-29 22:08:59 +0200

    Really fix the translations
    - remove the extra Messages.sh, leave just one in place (the more
      complete version which captures just the proper sources);
    - remove a useless line from the remaining Messages.sh (probably the
      first line can be removed as well);
    - set the translation domain for a library through a compilation flag.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find . -name \*.html` >> html.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/kio-stash.pot
+$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -name \*.qml` -o $podir/kio5_stash.pot
